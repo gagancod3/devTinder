7953d186f5edead3dfd70ef19c92739dcb46f9c6
gagancod3 <gagandeepnayyar@gmail.com>
2025-05-26 17:50:30 +0530

    user validation part 1

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Schema
 const userSchema = mongoose.Schema({
-    firstName: {
-        type: String
-    },
-    lastName: {
-        type: String
-    },
-    emailId: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    age:{
-        type: Number
-    },
-    gender:{
-        type: String
+  firstName: {
+    type: String,
+    required: true, // validation - mandatory field
+    minLength: 4,
+    maxLength: 50,
+},
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+    required: true,
+    unique: true, // only unique key can be inserted
+    lowercase: true, // converts value to lowercase
+    trim: true, // removes white spaces
+},
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 18,
+  },
+  gender: {
+    type: String,
+    // custom validation function
+    validate(value){
+        if(!["male","female","others"].includes(value)){
+            throw new Error("Gender data is not valid");
+        }
     }
+  },
+  photoUrl: {
+    type: String,
+    default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.citypng.com%2Fphoto%2F19313%2Fdownload-black-male-user-profile-icon-png&psig=AOvVaw0HOXkXXHpXc4fnko1Ny96u&ust=1748347272481000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwiK152ii8GNAxXD7DgGHfdaFEQQjRx6BAgAEBk"
+  },
+  about: {
+    type: String,
+    default: "This is a default about description of the user!"
+  },
+  skills: {
+    type: [String],
+  },
 });
 
 //Model
-const userModel = mongoose.model("User",userSchema);
+const userModel = mongoose.model("User", userSchema);
 
 module.exports = userModel;
-
